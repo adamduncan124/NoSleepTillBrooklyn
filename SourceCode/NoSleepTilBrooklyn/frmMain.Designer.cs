b1ae934155b0fb444020a86af9fc363baa16272c
Adam Duncan <32540695+adamduncan124@users.noreply.github.com>
2017-10-05 07:59:48 -0400

    Add files via upload
    application source code

---
+++ SourceCode/NoSleepTilBrooklyn/frmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace NoSleepTilBrooklyn
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnAction = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.chkPlayMusic = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnAction
+            // 
+            this.btnAction.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnAction.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.btnAction.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnAction.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAction.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAction.Location = new System.Drawing.Point(45, 46);
+            this.btnAction.Margin = new System.Windows.Forms.Padding(0);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(104, 36);
+            this.btnAction.TabIndex = 0;
+            this.btnAction.Text = "btnAction";
+            this.btnAction.UseVisualStyleBackColor = false;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblMessage.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblMessage.Location = new System.Drawing.Point(22, 12);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(76, 13);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "No Sleep Till...";
+            // 
+            // chkPlayMusic
+            // 
+            this.chkPlayMusic.AutoSize = true;
+            this.chkPlayMusic.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.chkPlayMusic.Location = new System.Drawing.Point(167, 55);
+            this.chkPlayMusic.Name = "chkPlayMusic";
+            this.chkPlayMusic.Size = new System.Drawing.Size(77, 17);
+            this.chkPlayMusic.TabIndex = 2;
+            this.chkPlayMusic.Text = "Play Music";
+            this.chkPlayMusic.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(284, 94);
+            this.Controls.Add(this.chkPlayMusic);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnAction);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "No Sleep Till Brooklyn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAction;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.CheckBox chkPlayMusic;
+    }
+}
+
---
+++ SourceCode/NoSleepTilBrooklyn/frmMain.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace NoSleepTilBrooklyn
+{
+    public partial class frmMain : Form
+    {
+        private const string MusicRelativePath = "music\\NoSleep.wav";
+
+        /*private int minX => 10;
+        private int minY => 10;
+        private int maxX => 300;
+        private int maxY => 300;
+        private int jump => 15;*/
+        private int? curX { get; set; }
+
+        private int? curY { get; set; }
+        private int maxCount => 50;
+        private int count { get; set; } = 0;
+        private bool stopLoop { get; set; } = true;
+        private SoundPlayer PlayAudio { get; set; }
+        private bool isPlaying { get; set; } = false;
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+            btnAction.Text = "Start Trip";
+            lblMessage.Text = "Click the Button to Begin the Trip.";
+        }
+
+        private void btnAction_Click(object sender, EventArgs e)
+        {
+            if (stopLoop)
+            {
+                btnAction.Text = "Stop And Sleep";
+                StartMouseLocation();
+            }
+            else
+            {
+                lblMessage.Text = "";
+                btnAction.Text = "Start Trip";
+                StopMouseLocation();
+            }
+        }
+
+        private void StartMouseLocation()
+        {
+            stopLoop = false;
+            LoopMouseLocation();
+        }
+
+        private void StopMouseLocation()
+        {
+            stopLoop = true;
+        }
+
+        private void LoopMouseLocation()
+        {
+            if (stopLoop)
+            {
+                lblMessage.Text = "You're Awake!!!";
+                StopMusic();
+                return;
+            }
+
+            Application.DoEvents();
+            Thread.Sleep(1000);
+            Application.DoEvents();
+
+            TestMouseAndMove();
+            LoopMouseLocation(); //call self
+            Application.DoEvents();
+        }
+
+        private void TestMouseAndMove()
+        {
+            if (stopLoop)
+            {
+                count = 0; //reset
+                return;
+            }
+
+            count++;
+
+            //set variables if null
+            if ((curX == null) && (curY == null))
+            {
+                curX = Cursor.Position.X;
+                curY = Cursor.Position.Y;
+            }
+
+            //first test mouse position is same            
+            if ((curX != Cursor.Position.X) || (curY != Cursor.Position.Y))
+            {
+                count = 0; //reset
+                curX = Cursor.Position.X;
+                curY = Cursor.Position.Y;
+                lblMessage.Text = "Mouse Moved!";
+                StopMusic();
+                return;
+            }
+
+            if (!isPlaying) PlayMusic();
+
+            lblMessage.Text = $"Key Press for No Sleep In {maxCount - count} seconds";
+
+            //next test count
+            if (count >= maxCount)
+            {
+                F15Press();
+                count = 0; //reset
+            }
+        }
+
+        private void F15Press()
+        {
+            SendKeys.Send("{F15}");
+        }
+
+        /*private void MoveMouseNextSpot()
+        {
+            //reset if needed
+            curX = (curX >= maxX) ? minX : 
+                ((curX < minX) ? minX : curX);
+            curY = (curY >= maxY) ? minY :
+                ((curY < minY) ? minY : curY);
+
+            //now jump!
+            curX = curX + jump;
+            curY = curY + jump;
+
+            if((curX != null) && (curY != null))
+                Cursor.Position = new Point((int) curX, (int) curY);
+        }*/
+
+        private void StopMusic()
+        {
+            PlayAudio?.Stop();
+            isPlaying = false;
+        }
+
+        private void PlayMusic()
+        {
+            var musicFile = $"{Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}\\{MusicRelativePath}";
+
+            if (!chkPlayMusic.Checked || !File.Exists(musicFile)) return;
+
+            if (PlayAudio == null)
+                PlayAudio = new SoundPlayer();
+
+            PlayAudio.SoundLocation = musicFile;
+            PlayAudio.Play();
+            isPlaying = true;
+        }
+    }
+}
